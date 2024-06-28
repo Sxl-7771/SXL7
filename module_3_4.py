2e2cf7bccd864a911344d5cb99d3045c0a6e8e07
Sxl-7771 <sxl7@mail.ru>
2024-06-28 23:25:02 +0300

    module_3_5

--- module_3_4.py
+++ module_3_4.py
@@ -6,6 +6,7 @@ def single_root_words(root_words, *other_words ):
 
     return same_words
 
+
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
