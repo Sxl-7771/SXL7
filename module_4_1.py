74b4ca9324dd4f1efdfd58de8048a60d83e28fc1
Sxl-7771 <sxl7@mail.ru>
2024-07-03 21:50:52 +0300

    module4_1

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fake_module
+import true_math as true_module
+
+
+result1 = fake_module.divide(69, 3)
+result2 = fake_module.divide(3, 0)
+result3 = true_module.divide(49, 7)
+result4 = true_module.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
