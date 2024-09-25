6a3697193dd446e5d5ee8394f45103aaa9f0a482
Sxl-7771 <sxl7@mail.ru>
2024-09-25 21:45:06 +0300

    module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,7 +1,11 @@
-def single_root_words(root_words, *other_words ):
+def single_root_words(root_word, *other_words):
+    root_word_lower = root_word.lower()
     same_words = []
+
     for word in other_words:
-        if word.lower() in root_words.lower() or root_words.lower() in word.lower():
+        word_lower = word.lower()
+
+        if root_word_lower in word_lower or word_lower in root_word_lower:
             same_words.append(word)
 
     return same_words
@@ -9,5 +13,6 @@ def single_root_words(root_words, *other_words ):
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
-print(result1)
-print(result2)
+
+print(result1)  # ['richiest', 'orichalcum', 'richies']
+print(result2)  # ['Able', 'Disable']
