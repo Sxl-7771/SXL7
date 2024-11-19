b49e383193dbd758823d03434b436c5162a4cf14
Sxl-7771 <sxl7@mail.ru>
2024-11-20 00:10:09 +0300

    Обновлённый проект

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from routers.task import router as task_router
-from routers.user import router as user_router
+from app.routers.task import router as task_router
+from app.routers.user import router as user_router
+from app.backend.db import Base, engine
+from app.models.task import Task
+from app.models.user import User
+
 
 app = FastAPI()
 
@@ -11,3 +15,12 @@ async def read_root():
 
 app.include_router(task_router)
 app.include_router(user_router)
+
+Base.metadata.create_all(bind=engine)
+
+print("SQL для User:")
+print(User.__table__.compile(compile_kwargs={"literal_binds": True}))
+
+print("\nSQL для Task:")
+print(Task.__table__.compile(compile_kwargs={"literal_binds": True}))
+
